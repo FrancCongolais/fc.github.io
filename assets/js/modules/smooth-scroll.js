b7ffea9bf1534a0bf72c3cb9c8549284458f77dd
FrancCongolais <franc.congolais.fc@gmail.com>
2025-11-26 01:30:14 +0200

    Update smooth-scroll.js

---
+++ assets/js/modules/smooth-scroll.js
@@ -0,0 +1,8 @@
+// smooth-scroll.js
+document.addEventListener('click', (e) => {
+  const a = e.target.closest('a[href^="#"]');
+  if (!a) return;
+  e.preventDefault();
+  const target = document.querySelector(a.getAttribute('href'));
+  if (target) target.scrollIntoView({ behavior: 'smooth' });
+});
