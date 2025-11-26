662426124ad7efe1f52bc4fbbd0276a9dcfe7aef
FrancCongolais <franc.congolais.fc@gmail.com>
2025-11-26 13:59:36 +0200

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-import initParticles from './particles.js';
-import './modules/navbar-toggle.js';
-import './modules/smooth-scroll.js';
-
-// Init particles
-document.addEventListener('DOMContentLoaded', () => {
-  initParticles();
-});
